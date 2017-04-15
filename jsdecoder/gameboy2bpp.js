ba92c2debe482ae173f921679f585ad1b11dc664
mofosyne <mofosyne@gmail.com>
2017-04-15 22:09:11 +1000

    yay it works!

--- jsdecoder/gameboy2bpp.js
+++ jsdecoder/gameboy2bpp.js
@@ -1,8 +1,14 @@
 window.onload = function() {
-    width = 8;
-    height = 8;
-    square_width = document.getElementById("demo_canvas").width / (width);
-    square_height = document.getElementById("demo_canvas").height / (height);
+    // Tile Constants
+    TILE_PIXEL_WIDTH = 8;
+    TILE_PIXEL_HEIGHT = 8;
+
+    // Gameboy Printer Tile Constant
+    TILES_PER_LINE = 20;
+    LINES_PER_IMAGE = 40;
+
+    square_width = document.getElementById("demo_canvas").width / (TILE_PIXEL_WIDTH * TILES_PER_LINE);
+    square_height = document.getElementById("demo_canvas").height / (TILE_PIXEL_HEIGHT * LINES_PER_IMAGE);
 
     colors = new Array("#ffffff", "#aaaaaa", "#555555", "#000000");
 
@@ -27,15 +33,18 @@ function refresh(canvas, rawBytes)
 
     console.log(tiles_rawBytes_array);
 
-    for (var i = 0; i < tiles_rawBytes_array.length; i++) 
+    for (var tile_i = 0; tile_i < tiles_rawBytes_array.length; tile_i++) 
     {   // Process each gameboy tile
 
-        pixels = decode(tiles_rawBytes_array[i]);
-        //console.log(pixels);
+        tile_x_offset = tile_i % TILES_PER_LINE;
+        tile_y_offset = Math.floor(tile_i / TILES_PER_LINE);
+
+        pixels = decode(tiles_rawBytes_array[tile_i]);
+        // console.log(pixels);
         if (pixels) {
-            paint(canvas, pixels, square_width, square_height );
+            paint(canvas, pixels, square_width, square_height, tile_x_offset, tile_y_offset);
         } else {
-            data.style.backgroundColor = "red";
+            data.style.backgroundColor = "red"; // Trigger error status
         }
     }
 }
@@ -50,32 +59,42 @@ function decode(rawBytes)
         byteArray[i] = parseInt(bytes.substr(i*2, 2), 16);
     }
 
-    var pixels = new Array(width*height);
-    for (var j = 0; j < height; j++) {
-        for (var i = 0; i < width; i++) {
+    var pixels = new Array(TILE_PIXEL_WIDTH*TILE_PIXEL_HEIGHT);
+    for (var j = 0; j < TILE_PIXEL_HEIGHT; j++) {
+        for (var i = 0; i < TILE_PIXEL_WIDTH; i++) {
             var hiBit = (byteArray[j*2 + 1] >> (7-i)) & 1;
             var loBit = (byteArray[j*2] >> (7-i)) & 1;
-            pixels[j*width + i] = (hiBit << 1) | loBit;
+            pixels[j*TILE_PIXEL_WIDTH + i] = (hiBit << 1) | loBit;
         }
     }
     return pixels;
 }
 
-function paint(canvas, pixels, pixel_width, pixel_height)
-{   // 
-    var ctx = canvas.getContext("2d");
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
+function paint(canvas, pixels, pixel_width, pixel_height, tile_x_offset, tile_y_offset )
+{   // This paints the tile
 
-    for (var i = 0; i < width; i++) 
-    {   // Iterate along the x image axis
-        for (var j = 0; j < height; j++) 
-        {   // Iterate along the y image axis
+    tile_offset     = tile_x_offset * tile_y_offset;
+    pixel_x_offset  = TILE_PIXEL_WIDTH   * tile_x_offset * pixel_width;
+    pixel_y_offset  = TILE_PIXEL_HEIGHT  * tile_y_offset * pixel_height;
+
+    var ctx = canvas.getContext("2d");
+    //ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    for (var i = 0; i < TILE_PIXEL_WIDTH; i++) 
+    {   // pixels along the tile's x axis
+        for (var j = 0; j < TILE_PIXEL_HEIGHT; j++) 
+        {   // pixels along the tile's y axis
 
             // Pixel Color
-            ctx.fillStyle = colors[pixels[j*width + i]];
+            ctx.fillStyle = colors[pixels[j*TILE_PIXEL_WIDTH + i]];
 
             // Pixel Position
-            ctx.fillRect(i*pixel_width, j*pixel_height, pixel_width, pixel_height);
+            ctx.fillRect(
+                    pixel_x_offset + i*pixel_width, 
+                    pixel_y_offset + j*pixel_height, 
+                    pixel_width, 
+                    pixel_height
+                );
         }
     }
 }
