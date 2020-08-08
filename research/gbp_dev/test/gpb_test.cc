acc940b840d4d06ffa7386a803985020e5492220
Brian Khuu <mofosyne@gmail.com>
2020-08-08 23:39:24 +1000

    vector mismatch check

--- research/gbp_dev/test/gpb_test.cc
+++ research/gbp_dev/test/gpb_test.cc
@@ -5,12 +5,33 @@
 #include "../gameboy_printer_protocol.h"
 #include "gpb_serial_io.h"
 
+
+/*******************************************************************************
+ * Test Vectors Variables
+*******************************************************************************/
 uint8_t testVector[] = {
   #include "2020-08-02_GameboyPocketCameraJP.txt"
 };
 
 uint8_t testResponse[sizeof(testVector)+100] = {0};
 
+typedef struct
+{
+  size_t startOfPacketBytePos;
+  size_t packetMismatch; // Byte parsed does not match test vector
+  size_t statusMismatch; // Status byte received does not match test vector
+  uint8_t printerID;
+  uint8_t printerIDExpected;
+  uint8_t status;
+  uint8_t statusExpected;
+} packetResult_t;
+
+packetResult_t testPacketResult[sizeof(testVector)] = {{0}};
+
+
+/*******************************************************************************
+ * Variable for gbp
+*******************************************************************************/
 uint8_t gbp_buffer[sizeof(testVector)+100] = {0};
 
 
@@ -106,7 +127,7 @@ int main(void)
           pktDataLength = 0;
           pktDataLength |= ((uint16_t)dataLengthByte0<<0)&0x00FF;
           pktDataLength |= ((uint16_t)dataLengthByte1<<8)&0xFF00;
-          printf("\r\n/* %02X : %s */\r\n", pktTotalCount, gbpCommand_toStr(commandTypeByte));
+          printf("/* %02X : %s */\r\n", pktTotalCount, gbpCommand_toStr(commandTypeByte));
         }
         // Print Hex Byte
         uint8_t rxByte = gbp_dataBuff_getByte();
@@ -137,6 +158,36 @@ int main(void)
             printf(" */");
           }
         }
+        // Check if packet bytes match test vector
+        //testPacketResult[pktTotalCount]
+        if (pktByteIndex < (8+pktDataLength))
+        {
+          if (pktByteIndex == 0)
+          {
+            testPacketResult[pktTotalCount].startOfPacketBytePos = byteTotal;
+          }
+          // Check if we have captured the byte correctly
+          if (rxByte != testVector[byteTotal])
+          {
+            testPacketResult[pktTotalCount].packetMismatch = true;
+          }
+        }
+        else if (pktByteIndex == (8+pktDataLength + 0))
+        {
+          // Do not care about status byte relating to printer ID
+          testPacketResult[pktTotalCount].printerID = testResponse[byteTotal];
+          testPacketResult[pktTotalCount].printerIDExpected = testVector[byteTotal];
+        }
+        else if (pktByteIndex == (8+pktDataLength + 1))
+        {
+          // Check if our status reply is correct
+          testPacketResult[pktTotalCount].status = testResponse[byteTotal];
+          testPacketResult[pktTotalCount].statusExpected = testVector[byteTotal];
+          if (testResponse[byteTotal] != testVector[byteTotal])
+          {
+            testPacketResult[pktTotalCount].statusMismatch = true;
+          }
+        }
         // Splitting packets for convenience
         if ((pktByteIndex>5)&&(pktByteIndex>=(9+pktDataLength)))
         {
@@ -153,6 +204,50 @@ int main(void)
       }
     }
   }
+
+
+#if 1 // Packet Mismatch
+  printf("\r\n/* testResponse Dump */\r\n");
+  for (size_t i = 0 ; i < (sizeof(testPacketResult)/sizeof(testPacketResult[0])) ; i++)
+  {
+    if (testPacketResult[i].packetMismatch || testPacketResult[i].statusMismatch)
+    {
+      printf("/*  Test Vector Error for packet %02X %s%s (ID: Got=0x%02X, Exp=0x%02X) ",
+          (unsigned int) i,
+          (char *) testPacketResult[i].packetMismatch ? "[Packet Mismatch]" : "",
+          (char *) testPacketResult[i].statusMismatch ? "[Status Mismatch]" : "",
+          (unsigned) testPacketResult[i].printerID,
+          (unsigned) testPacketResult[i].printerIDExpected
+        );
+      printf("(Status: Got=");
+      if (testPacketResult[i].status)
+      {
+        printf((testPacketResult[i].status & GBP_STATUS_MASK_LOWBAT) ? "LOWBAT " : "");
+        printf((testPacketResult[i].status & GBP_STATUS_MASK_ER2   ) ? "ER2 "    : "");
+        printf((testPacketResult[i].status & GBP_STATUS_MASK_ER1   ) ? "ER1 "    : "");
+        printf((testPacketResult[i].status & GBP_STATUS_MASK_ER0   ) ? "ER0 "    : "");
+        printf((testPacketResult[i].status & GBP_STATUS_MASK_UNTRAN) ? "UNTRAN"  : "");
+        printf((testPacketResult[i].status & GBP_STATUS_MASK_FULL  ) ? "FULL "   : "");
+        printf((testPacketResult[i].status & GBP_STATUS_MASK_BUSY  ) ? "BUSY "   : "");
+        printf((testPacketResult[i].status & GBP_STATUS_MASK_SUM   ) ? "SUM "    : "");
+      }
+      printf(", Exp=");
+      if (testPacketResult[i].statusExpected)
+      {
+        printf((testPacketResult[i].statusExpected & GBP_STATUS_MASK_LOWBAT) ? "LOWBAT " : "");
+        printf((testPacketResult[i].statusExpected & GBP_STATUS_MASK_ER2   ) ? "ER2 "    : "");
+        printf((testPacketResult[i].statusExpected & GBP_STATUS_MASK_ER1   ) ? "ER1 "    : "");
+        printf((testPacketResult[i].statusExpected & GBP_STATUS_MASK_ER0   ) ? "ER0 "    : "");
+        printf((testPacketResult[i].statusExpected & GBP_STATUS_MASK_UNTRAN) ? "UNTRAN"  : "");
+        printf((testPacketResult[i].statusExpected & GBP_STATUS_MASK_FULL  ) ? "FULL "   : "");
+        printf((testPacketResult[i].statusExpected & GBP_STATUS_MASK_BUSY  ) ? "BUSY "   : "");
+        printf((testPacketResult[i].statusExpected & GBP_STATUS_MASK_SUM   ) ? "SUM "    : "");
+      }
+      printf(")");
+      printf("*/\r\n");
+    }
+  }
+#endif
 
 #if 0 // Dev: testResponse Dump
   printf("\r\n/* testResponse Dump */\r\n");
