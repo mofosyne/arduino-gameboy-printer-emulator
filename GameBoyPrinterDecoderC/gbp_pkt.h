0704e88f65b084a7d619e213842c2eb08621c51a
Brian <mofosyne@gmail.com>
2021-05-24 23:21:59 +1000

    WIP C decoder

---
+++ GameBoyPrinterDecoderC/gbp_pkt.h
@@ -0,0 +1,103 @@
+/*************************************************************************
+ *
+ * Gameboy Printer Packet Parser
+ * Part of GAMEBOY PRINTER EMULATION PROJECT V2 (Arduino)
+ * Copyright (C) 2020 Brian Khuu
+ *
+ * PURPOSE: This module focus on parsing a stream of bytes as gbp packets
+ * LICENCE:
+ *   This file is part of Arduino Gameboy Printer Emulator.
+ *
+ *   Arduino Gameboy Printer Emulator is free software:
+ *   you can redistribute it and/or modify it under the terms of the
+ *   GNU General Public License as published by the Free Software Foundation,
+ *   either version 3 of the License, or (at your option) any later version.
+ *
+ *   Arduino Gameboy Printer Emulator is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with Arduino Gameboy Printer Emulator.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#include <stdint.h> // uint8_t
+#include <stddef.h> // size_t
+#include <stdbool.h> // bool
+#include "gameboy_printer_protocol.h"
+
+#define GBP_PKT_PAYLOAD_BUFF_SIZE_IN_BYTE GBP_TILE_SIZE_IN_BYTE
+
+typedef enum
+{
+  GBP_REC_NONE,
+  GBP_REC_GOT_PACKET,
+  /* Streaming Packet */
+  GBP_REC_GOT_PAYLOAD_PARTAL,
+  GBP_REC_GOT_PACKET_END
+} gbp_received_t;
+
+typedef struct
+{
+  gbp_received_t received;
+  uint16_t pktByteIndex;
+
+  /* Packet Information */
+  uint8_t command;
+  uint8_t compression;
+  uint16_t dataLength;
+  uint8_t printerID;
+  uint8_t status;
+
+  /* Decompressor */
+  size_t buffIndex;
+  bool compressedRun;
+  bool repeatByteGet;
+  uint8_t repeatByte;
+  uint8_t loopRunLength;
+
+} gbp_pkt_t;
+
+typedef struct
+{
+  // This is the tile data accumulator
+  unsigned char count;
+  unsigned char tile[GBP_TILE_SIZE_IN_BYTE];
+} gbp_pkt_tileAcc_t;
+
+
+bool gbp_pkt_init(gbp_pkt_t *_pkt);
+bool gbp_pkt_reset(gbp_pkt_t *_pkt);
+bool gbp_pkt_processByte(gbp_pkt_t *_pkt,  const uint8_t _byte, uint8_t buffer[], uint8_t *bufferSize, const size_t bufferMax);
+bool gbp_pkt_decompressor(gbp_pkt_t *_pkt, const uint8_t buff[], const size_t buffSize, gbp_pkt_tileAcc_t *tileBuff);
+bool gbp_pkt_tileAccu_tileReadyCheck(gbp_pkt_tileAcc_t *tileBuff);
+
+/*******************************************************************************
+ * Print Instruction
+*******************************************************************************/
+
+static inline int gbp_pkt_printInstruction_num_of_sheets(uint8_t payloadBuff[GBP_PRINT_INSTRUCT_PAYLOAD_SIZE])
+{
+  return (payloadBuff[GBP_PRINT_INSTRUCT_INDEX_NUM_OF_SHEETS  ]);
+}
+
+static inline int gbp_pkt_printInstruction_num_of_linefeed_before_print(uint8_t payloadBuff[GBP_PRINT_INSTRUCT_PAYLOAD_SIZE])
+{
+  return (payloadBuff[GBP_PRINT_INSTRUCT_INDEX_NUM_OF_LINEFEED] >> 4) & 0x0F;
+}
+
+static inline int gbp_pkt_printInstruction_num_of_linefeed_after_print(uint8_t payloadBuff[GBP_PRINT_INSTRUCT_PAYLOAD_SIZE])
+{
+  return (payloadBuff[GBP_PRINT_INSTRUCT_INDEX_NUM_OF_LINEFEED]) & 0x0F;
+}
+
+static inline int gbp_pkt_printInstruction_palette_value(uint8_t payloadBuff[GBP_PRINT_INSTRUCT_PAYLOAD_SIZE])
+{
+  return (payloadBuff[GBP_PRINT_INSTRUCT_INDEX_PALETTE_VALUE  ]);
+}
+
+static inline int gbp_pkt_printInstruction_print_density(uint8_t payloadBuff[GBP_PRINT_INSTRUCT_PAYLOAD_SIZE])
+{
+  return (payloadBuff[GBP_PRINT_INSTRUCT_INDEX_PRINT_DENSITY  ]);
+}
---
+++ GameBoyPrinterDecoderC/gpbdecoder.cc
@@ -0,0 +1,257 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <stdbool.h>
+#include <getopt.h>
+#include <sys/types.h>
+
+#include <stdlib.h>
+
+#include "gameboy_printer_protocol.h"
+#include "gbp_pkt.h"
+
+
+/* The official name of this program (e.g., no 'g' prefix).  */
+#define PROGRAM_NAME "gpbdecoder"
+
+//static bool verbose_flag = false;
+
+char * ifilename = NULL;
+char * ofilename = NULL;
+FILE * ifilePtr = NULL;
+FILE * ofilePtr = NULL;
+
+
+void gbpdecoder_gotByte(const uint8_t byte);
+
+
+uint8_t pktCounter = 0; // Dev Varible
+//////
+gbp_pkt_t gbp_pktBuff = {GBP_REC_NONE, 0};
+uint8_t gbp_pktbuff[GBP_PKT_PAYLOAD_BUFF_SIZE_IN_BYTE] = {0};
+uint8_t gbp_pktbuffSize = 0;
+gbp_pkt_tileAcc_t tileBuff = {0};
+//////
+
+/*******************************************************************************
+ * Test Vectors Variables
+*******************************************************************************/
+const uint8_t testVector[] = {
+  //#include "2020-08-02_GameboyPocketCameraJP.txt" // Single Image
+  //#include "2020-08-02_PokemonSpeciallPicachuEdition.txt" // Mult-page Image
+  #include "./test/2020-08-10_Pokemon_trading_card_compressiontest.txt" // Compression
+};
+
+
+
+/*******************************************************************************
+ * Utilites
+*******************************************************************************/
+
+const char *gbpCommand_toStr(int val)
+{
+  switch (val)
+  {
+    case GBP_COMMAND_INIT    : return "INIT";
+    case GBP_COMMAND_PRINT   : return "PRNT";
+    case GBP_COMMAND_DATA    : return "DATA";
+    case GBP_COMMAND_BREAK   : return "BREK";
+    case GBP_COMMAND_INQUIRY : return "INQY";
+    default: return "?";
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+/*******************************************************************************
+ * Main Test Routine
+*******************************************************************************/
+int
+main (int argc, char **argv)
+{
+  (void) argc;
+  (void) argv;
+
+#if 1
+  int c;
+  static struct option const long_options[] =
+  {
+    /* These options set a flag. */
+    //{"verbose", no_argument, &verbose_flag, 1},
+    //{"brief",   no_argument, &verbose_flag, 0},
+    /* These options don’t set a flag.
+        We distinguish them by their indices. */
+    {"input", required_argument, NULL, 'i'},
+    {"output", required_argument, NULL, 'o'},
+    {"verbose", no_argument, NULL, 'v'},
+    {NULL, 0, NULL, 0}
+  };
+
+  while ((c = getopt_long (argc, argv, "o:i:", long_options, NULL))
+         != -1)
+  {
+    switch (c)
+    {
+        case 'i':
+          ifilename = optarg;
+          break;
+
+        case 'o':
+          ofilename = optarg;
+          break;
+    }
+  }
+
+  if (ifilename)
+  {
+    ifilePtr = fopen(ifilename, "r+");
+    if (ifilePtr)
+    {
+      printf ("file input `%s' open\n", ifilename);
+    }
+  }
+  if (ifilePtr == NULL)
+  {
+    ifilePtr = stdin;
+    printf ("file input stdin\n");
+  }
+
+  if (ofilename)
+  {
+    ofilePtr = fopen(ofilename, "w+");
+    if (ofilePtr)
+    {
+      printf ("file output `%s' open\n", ofilename);
+    }
+  }
+#endif
+
+
+  gbp_pkt_init(&gbp_pktBuff);
+
+  char ch = 0;
+  bool skipLine = false;
+  int  lowNibFound = 0;
+  uint8_t byte = 0;
+  unsigned int bytec = 0;
+  while ((ch = fgetc(ifilePtr)) != EOF)
+  {
+    // Skip Comments
+    if (ch == '/')
+    {
+      skipLine = true;
+      continue;
+    }
+    else if (skipLine)
+    {
+      if (ch == '\n')
+      {
+        skipLine = false;
+      }
+      continue;
+    }
+
+    // Parse Nib
+    char nib = -1;
+    if (('0' <= ch) && (ch <= '9'))
+    {
+      nib = ch - '0';
+    }
+    else if (('a' <= ch) && (ch <= 'f'))
+    {
+      nib = ch - 'a' + 10;
+    }
+    else if (('A' <= ch) && (ch <= 'F'))
+    {
+      nib = ch - 'A' + 10;
+    }
+
+    /* Parse As Byte */
+    bool byteFound = false;
+    // Hex Parse Edge Cases
+    if (lowNibFound)
+    {
+      // '0x' found. Ignore
+      if ((byte == 0) && (ch == 'x'))    
+        lowNibFound = false;
+      // Not a hex digit pair. Ignore
+      if (nib == -1) 
+        lowNibFound = false;
+    }
+    // Hex Byte Parsing
+    if (nib != -1)
+    {
+      if (!lowNibFound)
+      {
+        lowNibFound = true;
+        byte = nib << 4;
+      }
+      else
+      {
+        lowNibFound = false;
+        byte |= nib << 0;
+        byteFound = true;
+      }
+    }
+
+    // Byte Was Found
+    if (byteFound)
+    {
+      bytec++;
+      //printf("0x%02X %s", byte, ((bytec%16) == 0) ? "\r\n" : "");
+      // Decode
+      gbpdecoder_gotByte(byte);
+    }
+  }
+  return 0;
+}
+
+
+void gbpdecoder_gotByte(const uint8_t byte)
+{
+  if (gbp_pkt_processByte(&gbp_pktBuff, byte, gbp_pktbuff, &gbp_pktbuffSize, sizeof(gbp_pktbuff)))
+  {
+    if (gbp_pktBuff.received == GBP_REC_GOT_PACKET)
+    {
+      pktCounter++;
+      printf("// %s | compression: %1u, dlength: %3u, printerID: 0x%02X, status: %u | %d | ",
+          gbpCommand_toStr(gbp_pktBuff.command),
+          (unsigned) gbp_pktBuff.compression,
+          (unsigned) gbp_pktBuff.dataLength,
+          (unsigned) gbp_pktBuff.printerID,
+          (unsigned) gbp_pktBuff.status,
+          (unsigned) pktCounter
+        );
+      for (int i = 0 ; i < gbp_pktbuffSize ; i++)
+      {
+        printf("%02X ", gbp_pktbuff[i]);
+      }
+      printf("\r\n");
+    }
+    else
+    {
+      // Support compression payload
+      while (gbp_pkt_decompressor(&gbp_pktBuff, gbp_pktbuff, gbp_pktbuffSize, &tileBuff))
+      {
+        if (gbp_pkt_tileAccu_tileReadyCheck(&tileBuff))
+        {
+          // Got tile
+          for (int i = 0 ; i < GBP_TILE_SIZE_IN_BYTE ; i++)
+          {
+            printf("%02X ", tileBuff.tile[i]);
+          }
+          printf("\r\n");
+        }
+      }
+    }
+  }
+}
+
